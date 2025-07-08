c81210521b13cfefe953ff6a8d1b9ff47584e129
Shourya1009 <jshourya007@gmal.com>
2025-07-09 01:37:17 +0530

    Updated operator.js with comparison and logical operators

--- JAVASCRIPT_NOTES/operators.js
+++ JAVASCRIPT_NOTES/operators.js
@@ -30,3 +30,54 @@ a /= 2;
 console.log("Value of a after divison is : ", a);
 a **= 2;
 console.log("Value of a after exponent is : ", a);
+
+/*
+ 3] Comparison Operator -> use to compare two values and return a boolean value(true or false ) based on comparison.
+*/
+// equal to operator
+console.log("Equal to: 2 == 2 is", 2 == 2);
+
+// not equal operator
+console.log("Not equal to: 3 != 3 is", 3 != 3);
+
+// strictly equal to operator - > return true if  value and type matches otherwse return false.
+console.log("Strictly equal to: 2 === '2' is", 2 === "2");
+
+// strictly not equal to operator -> return true if  either the value or the type does not matches otherwise return false.
+console.log("Strictly not equal to: 2 !== '2' is", 2 !== "2");
+
+// greater than operator
+console.log("Greater than: 3 > 3 is", 3 > 3);
+
+// less than operator
+console.log("Less than: 2 > 2 is", 2 > 2);
+
+// greater than or equal to operator
+console.log("Greater than or equal to: 3 >= 3 is", 3 >= 3);
+
+// less than or equal to operator
+console.log("Less than or equal to: 2 <= 2 is", 2 <= 2);
+
+/*
+Logical Operator -> use to perform logical operations on boolean expressions.
+
+&& (Logical AND) -> expression1 && expression2 -> true only if both expression1 and expression2 are true
+
+|| (Logical OR)	-> expression1 || expression2 -> true if either expression1 or expression2 is true
+
+! (Logical NOT) -> !expression -> false if expression is true and vice versa
+*/
+
+let u = 3;
+
+// logical AND
+console.log(u < 5 && u > 0); // true
+console.log(u < 5 && u > 6); // false
+
+// logical OR
+console.log(u > 2 || u > 5); // true
+console.log(u > 3 || u < 0); // false
+
+// logical NOT
+console.log(!(u == 3)); // false
+console.log(!(u < 2)); // true
