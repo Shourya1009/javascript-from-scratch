ac932c1f73133c85b0978874f1be09c3a91f0c39
Shourya1009 <jshourya007@gmail.com>
2025-07-12 21:19:51 +0530

    Added if_else.js

---
+++ JAVASCRIPT_NOTES/if_else.js
@@ -0,0 +1,9 @@
+// The if...else statement is a conditional statement that executes a block of code only when a specific condition is met.
+
+let age = 10;
+
+if (age < 18) {
+  console.log("You are not eligible to vote");
+} else {
+  console.log("You are eligible to vote");
+}
